5d05fed0ce4466d9f3e83b493fb4bd44b2afbe52
Ravi191203 <raviraghavendrags@gmail.com>
2024-05-01 16:24:28 +0530

    Done

---
+++ app/src/main/java/com/example/bug_busters/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.bug_busters;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        // Set custom tab layout for each tab
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            if (tab != null) {
+                tab.setCustomView(R.layout.custom_tab_layout);
+            }
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ImageFragment(R.drawable.image1), "Tab 1");
+        adapter.addFragment(new ImageFragment(R.drawable.image2), "Tab 2");
+        adapter.addFragment(new ImageFragment(R.drawable.image3), "Tab 3");
+        viewPager.setAdapter(adapter);
+    }
+}
